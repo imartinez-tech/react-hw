d989b3588ff96b1b9515d27f548bf570b32c04d0
ibraim martinez <ibraimm5@gmail.com>
2021-10-19 22:45:47 -0400

    importing and exporting components

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React from 'react'; 
+import Navbar from './components/navbar';
+import About from './components/about';
+import Contact from './components/contact';
+
 
 export default function App() {
     return (
