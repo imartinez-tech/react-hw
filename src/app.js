e1c6c834d1bd2552aba27435a228e3e62924aba8
ibraim martinez <ibraimm5@gmail.com>
2021-10-19 22:42:43 -0400

    creating basic file structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+export default function App() {
+    return (
+        <main>
+            <Navbar />
+            <About />
+            <Resume /> 
+            <Contact /> 
+        </main>
+    )
+}
